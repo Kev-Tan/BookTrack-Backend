a1725f5a7df3b4eb261a16619edc4e04d4d17124
Kev-Tan <kevinsutan99@gmail.com>
2026-03-04 03:01:57 +0800

    Create two functions using Gemini
    - Create first function to give the genres given an array of books
    - Create a second function to recommend books given a prompt
    - Formatted the output of the functions accordingly

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import httpx
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
+from google import genai
+from google.genai.types import GenerateContentConfig
+from typing import List, Optional
+from pydantic import BaseModel, Field
+import ast
+import json
 
-load_dotenv()
-key = os.getenv("GOOGLE_BOOKS_API_KEY")
+
+
+
 
 app = FastAPI()
-
+load_dotenv()
+google_books_key = os.getenv("GOOGLE_BOOKS_API_KEY")
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ["http://localhost:5173"],
@@ -18,12 +27,60 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-EXTERNAL_API_URL =  "https://www.googleapis.com/books/v1/volumes"
 client = httpx.AsyncClient()
+genai_client = genai.Client(api_key=gemini_api_key)
+
+
+EXTERNAL_API_URL =  "https://www.googleapis.com/books/v1/volumes"
 
 
 @app.get("/books/{book_name}")
 async def find_book(book_name: str):
-    response = await client.get(f"{EXTERNAL_API_URL}?q=intitle:{book_name}&key={key}")
+    response = await client.get(f"{EXTERNAL_API_URL}?q=intitle:{book_name}&key={google_books_key}")
     return response.json()
+
+class Books_Genre(BaseModel):
+    genres: List[str]
     
+class Book(BaseModel): # Inherit from BaseModel
+    title: str
+    author: str
+    synopsis: str
+
+class BooksList(BaseModel): # Inherit from BaseModel
+    books: List[Book]
+
+@app.get("/recommend_books/")
+async def get_recommendation(prompt: str):
+    config = GenerateContentConfig(
+        temperature=0.7,
+        system_instruction="You are a helpful assistant that provides concise answers. In this case, please recommend the three most appropriate books based on the user's prompt",
+        response_mime_type="application/json",
+        response_schema=BooksList,
+    )
+    
+    response = genai_client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=[prompt],
+        config=config,
+    )
+    
+    print(response)
+    return json.loads(response.text)
+
+@app.get("/classify")
+async def response(prompt: str):
+    config = GenerateContentConfig(
+        temperature=0.7,
+        system_instruction="You are a helpful assistant that provides concise answers. In this case, please classify the genre of the books given to you. You have [Romance, Thriller, Drama, Horror, Comedy, Philosophy, Memoir, Autobiography, Biography, Educational, Others]. Give the genres of the books fed into you in order",
+        response_mime_type="application/json",
+        response_schema=Books_Genre,
+    )
+    
+    response = genai_client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=[prompt],
+        config=config,
+    )
+    print(response)
+    return response.text
